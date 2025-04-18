ea610296fa03a4920ba1c8ab8631e8d86047fb9a
听风 <73208700+hyue418@users.noreply.github.com>
2025-04-18 14:21:00 +0800

    Update oray-redirect.user.js
    feat:优化路径匹配

--- oray-redirect.user.js
+++ oray-redirect.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         花生壳自动跳转
 // @namespace    https://github.com/hyue418
-// @version      1.0.2
+// @version      1.0.3
 // @description  自动跳过恼人的花生壳中间页
 // @author       hyue418
 // @icon         https://passport.oray.com/favicon.ico
-// @match        *
+// @match        http*://*/*
 // @downloadURL  https://ghfast.top/https://raw.githubusercontent.com/hyue418/oray-redirect/master/oray-redirect.user.js
 // @updateURL    https://ghfast.top/https://raw.githubusercontent.com/hyue418/oray-redirect/master/oray-redirect.user.js
 // ==/UserScript==
@@ -14,6 +14,6 @@
     'use strict';
     var searchText = "如需去除此页，请联系管理员升级服务版本";
     if (document.body.innerText.includes(searchText)) {
-        location.reload();
+        window.location.href = window.location.origin;
     }
 })();
