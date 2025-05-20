48e7818feef88de90dbb72e2aba7b1925138946f
听风 <73208700+hyue418@users.noreply.github.com>
2025-05-20 14:58:25 +0800

    feat:同步花生壳中间页逻辑更新

--- oray-redirect.user.js
+++ oray-redirect.user.js
@@ -1,23 +1,27 @@
 // ==UserScript==
 // @name         花生壳自动跳转
 // @namespace    https://github.com/hyue418
-// @version      1.0.6
+// @version      1.0.7
 // @description  自动跳过恼人的花生壳中间页
 // @author       hyue418
 // @icon         https://passport.oray.com/favicon.ico
-// @match        http*://*/*
+// @match        https://access-hsk.oray.com/loading*
+// @match        https://passport.oray.com/login/hsk*
 // @downloadURL  https://ghfast.top/https://raw.githubusercontent.com/hyue418/oray-redirect/master/oray-redirect.user.js
 // @updateURL    https://ghfast.top/https://raw.githubusercontent.com/hyue418/oray-redirect/master/oray-redirect.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    const intervalId = setInterval(() => {
-        if (document.title.includes("花生壳")) {
-            var searchText = "如需去除此页，请联系管理员升级服务版本";
-            if (document.body.innerText.includes(searchText)) {
-                window.location.href = window.location.origin;
-            }
-        }
-    }, 300);
+    const r = new URLSearchParams(window.location.search).get('r');
+    if (r) {
+        location.replace(decodeURIComponent(r))
+        return;
+    };
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);
+    if (url.pathname === '/login/hsk'&&url.searchParams.has('s_url')) {
+        url.searchParams.delete('s_url');
+        window.location.href = url.toString();
+    }
 })();
